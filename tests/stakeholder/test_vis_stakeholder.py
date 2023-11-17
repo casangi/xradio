813a4a5fbc72cfdbd521d15f51a0111bc287962d
Jan-Willem <jwsteeb@gmail.com>
2023-11-17 10:28:12 -0500

    black

--- tests/stakeholder/test_vis_stakeholder.py
+++ tests/stakeholder/test_vis_stakeholder.py
@@ -1,12 +1,17 @@
-from xradio.vis import read_processing_set, load_processing_set, convert_msv2_to_processing_set
+from xradio.vis import (
+    read_processing_set,
+    load_processing_set,
+    convert_msv2_to_processing_set,
+)
 from xradio.data.datasets import download
 import numpy as np
 import pytest
 import os
 
-relative_tolerance = 10**(-12)
+relative_tolerance = 10 ** (-12)
 
-def base_test(msv2_name,expected_sum_value):
+
+def base_test(msv2_name, expected_sum_value):
     download(file=msv2_name, source="dropbox")
     ps_name = msv2_name[:-3] + ".vis.zarr"
     convert_msv2_to_processing_set(
@@ -24,32 +29,43 @@ def base_test(msv2_name,expected_sum_value):
     sum = 0.0
     sum_lazy = 0.0
     for ms_xds_name in ps.keys():
-        sum = sum + np.nansum(np.abs(ps[ms_xds_name].VISIBILITY*ps[ms_xds_name].WEIGHT))
-        sum_lazy = sum_lazy + np.nansum(np.abs(ps_lazy[ms_xds_name].VISIBILITY*ps_lazy[ms_xds_name].WEIGHT))
+        sum = sum + np.nansum(
+            np.abs(ps[ms_xds_name].VISIBILITY * ps[ms_xds_name].WEIGHT)
+        )
+        sum_lazy = sum_lazy + np.nansum(
+            np.abs(ps_lazy[ms_xds_name].VISIBILITY * ps_lazy[ms_xds_name].WEIGHT)
+        )
 
     print(sum)
 
-    os.system('rm -rf ' + msv2_name)
-    os.system('rm -rf ' + ps_name)
+    os.system("rm -rf " + msv2_name)
+    os.system("rm -rf " + ps_name)
 
-    assert sum==sum_lazy, "read_processing_set and load_processing_set VISIBILITY and WEIGHT values differ."
-    assert sum==pytest.approx(expected_sum_value, rel=relative_tolerance), "VISIBILITY and WEIGHT values have changed."
+    assert (
+        sum == sum_lazy
+    ), "read_processing_set and load_processing_set VISIBILITY and WEIGHT values differ."
+    assert sum == pytest.approx(
+        expected_sum_value, rel=relative_tolerance
+    ), "VISIBILITY and WEIGHT values have changed."
 
 
 def test_alma():
-    base_test("Antennae_North.cal.lsrk.split.ms",190.0405216217041)
+    base_test("Antennae_North.cal.lsrk.split.ms", 190.0405216217041)
+
 
 def test_ska_mid():
-    base_test("AA2-Mid-sim_00000.ms",551412.3125)
+    base_test("AA2-Mid-sim_00000.ms", 551412.3125)
+
 
 def test_lofar():
-    base_test("small_lofar.ms",10345086189568.0)
+    base_test("small_lofar.ms", 10345086189568.0)
+
 
 def test_meerkat():
-    base_test("small_meerkat.ms",333866268.0)
+    base_test("small_meerkat.ms", 333866268.0)
+
 
 # test_alma()
 # test_ska_mid()
 # test_lofar()
 # test_meerkat()
-

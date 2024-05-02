9e39ad5a78224c7cc7a8577bf9b5936844f861aa
Peter Wortmann <peter.wortmann@skao.int>
2024-05-02 11:17:51 +0100

    Some slight documentation re-organisation

--- src/xradio/vis/__init__.py
+++ src/xradio/vis/__init__.py
@@ -4,9 +4,13 @@ from .convert_msv2_to_processing_set import convert_msv2_to_processing_set
 
 from .vis_io import read_vis, load_vis_block, write_vis
 
-from .model import VisibilityXds
+from .schema import VisibilityXds
 
 __all__ = [
-    "read_vis", "load_vis_block", "write_vis"
+    "read_processing_set",
+    "load_processing_set",
+    "convert_msv2_to_processing_set",
     "VisibilityXds",
+    "PointingXds",
+    "AntennaXds",
 ]

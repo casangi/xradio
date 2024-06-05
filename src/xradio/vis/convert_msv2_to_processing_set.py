148a87a77af36aa852e0c75d42f78451615da5df
Federico Montesino Pouzols <fpouzols@eso.org>
2024-06-05 10:46:34 +0200

    update doc chunksize params (can also take float (GiB)), re #130

--- src/xradio/vis/convert_msv2_to_processing_set.py
+++ src/xradio/vis/convert_msv2_to_processing_set.py
@@ -14,9 +14,9 @@ def convert_msv2_to_processing_set(
     in_file: str,
     out_file: str,
     partition_scheme: {"ddi_intent_field", "ddi_state_field"} = "ddi_intent_field",
-    main_chunksize: Union[Dict, str, None] = None,
+    main_chunksize: Union[Dict, float, None] = None,
     with_pointing: bool = True,
-    pointing_chunksize: Union[Dict, str, None] = None,
+    pointing_chunksize: Union[Dict, float, None] = None,
     pointing_interpolate: bool = False,
     compressor: numcodecs.abc.Codec = numcodecs.Zstd(level=2),
     storage_backend="zarr",
@@ -35,12 +35,12 @@ def convert_msv2_to_processing_set(
         A MS v4 can only contain a single spectral window, polarization setup, intent, and field. Consequently, the MS v2 is partitioned when converting to MS v4.
         The partition_scheme "ddi_intent_field" gives the largest partition that meets the MS v4 specification. The partition_scheme "ddi_state_field" gives a finer granularity where the data is also partitioned by state (the state partitioning will ensure a single intent).
         By default, "ddi_intent_field".
-    main_chunksize : Union[Dict, str, None], optional
-        A dictionary that defines the chunk size of the main dataset. Acceptable keys are "time", "baseline", "antenna", "frequency", "polarization". By default, None.
+    main_chunksize : Union[Dict, float, None], optional
+        Defines the chunk size of the main dataset. If given as a dictionary, defines the sizes of several dimensions, and acceptable keys are "time", "baseline_id", "antenna_id", "frequency", "polarization". If given as a float, gives the size of a chunk in GiB. By default, None.
     with_pointing : bool, optional
         Whether to convert the POINTING subtable into pointing sub-datasets
-    pointing_chunksize : Union[Dict, str, None], optional
-        A dictionary that defines the chunk size of the pointing dataset. Acceptable keys are "time" and "antenna". By default, None.
+    pointing_chunksize : Union[Dict, float, None], optional
+        Defines the chunk size of the pointing dataset. If given as a dictionary, defines the sizes of several dimensions, acceptable keys are "time" and "antenna_id". If given as a float, defines the size of a chunk in GiB. By default, None.
     pointing_interpolate : bool, optional
         Whether to interpolate the time axis of the pointing sub-dataset to the time axis of the main dataset
     compressor : numcodecs.abc.Codec, optional

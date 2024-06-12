983ea601e7200f479eb096003eec8d2b55e4a496
Federico Montesino Pouzols <fpouzols@eso.org>
2024-06-12 18:37:24 +0200

    fix userWarning about duplicated dimensions, ~17 warnings

--- src/xradio/vis/_vis_utils/_ms/subtables.py
+++ src/xradio/vis/_vis_utils/_ms/subtables.py
@@ -13,7 +13,7 @@ from ._tables.read_subtables import read_ephemerides, read_delayed_pointing_tabl
 
 subt_rename_ids = {
     "ANTENNA": {"row": "antenna_id", "dim_1": "xyz"},
-    "FEED": {"dim_1": "xyz", "dim_2": "receptor", "dim_3": "receptor"},
+    "FEED": {"dim_1": "xyz", "dim_2": "receptor", "dim_3": "receptor2"},
     "FIELD": {"row": "field_id", "dim_1": "poly_id", "dim_2": "ra/dec"},
     "FREQ_OFFSET": {"antenna1": "antenna1_id", "antenna2": "antenna2_id"},
     "OBSERVATION": {"row": "observation_id", "dim_1": "start/end"},

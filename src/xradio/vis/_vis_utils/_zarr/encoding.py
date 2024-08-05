299ab72426e427271f20bd9f8d7bfa958f824e02
sstansill <sean.stansill@nag.com>
2024-08-05 14:15:50 +0100

    Update dataset encoding
    The else clause is never reached, it is handled in line 2

--- src/xradio/vis/_vis_utils/_zarr/encoding.py
+++ src/xradio/vis/_vis_utils/_zarr/encoding.py
@@ -6,9 +6,5 @@ def add_encoding(xds, compressor, chunks=None):
 
     encoding = {}
     for da_name in list(xds.data_vars):
-        if chunks:
-            da_chunks = [chunks[dim_name] for dim_name in xds[da_name].sizes]
-            xds[da_name].encoding = {"compressor": compressor, "chunks": da_chunks}
-            # print(xds[da_name].encoding)
-        else:
-            xds[da_name].encoding = {"compressor": compressor}
+        da_chunks = [chunks[dim_name] for dim_name in xds[da_name].sizes]
+        xds[da_name].encoding = {"compressor": compressor, "chunks": da_chunks}

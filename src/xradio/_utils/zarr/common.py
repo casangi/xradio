ed4af64c1d9fe81cc532615af6f03afd97f58aa6
Andrew Thomas <amcnicho@nrao.edu>
2024-08-05 16:33:59 -0400

    Handle tilde expansion in _get_ms_stores_and_file_system

--- src/xradio/_utils/zarr/common.py
+++ src/xradio/_utils/zarr/common.py
@@ -8,8 +8,11 @@ from botocore.exceptions import NoCredentialsError
 
 def _get_file_system_and_items(ps_store: str):
 
+    # default to assuming the data are accessible on local file system
     if os.path.isdir(ps_store):
-        # default to assuming the data are accessible on local file system
+        # handle a common shell convention
+        if ps_store.startswith("~"):
+            ps_store = os.path.expanduser(ps_store)
         items = os.listdir(ps_store)
         file_system = os
 

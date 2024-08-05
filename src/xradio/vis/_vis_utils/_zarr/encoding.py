6f2ca99923a33aaa30740a560d5912d25aa5ae8b
sstansill <sean.stansill@nag.com>
2024-08-05 14:18:02 +0100

    Update dataset encoding
    Ensure chunk encoding isn't specified when the DataArray is a dask array

--- src/xradio/vis/_vis_utils/_zarr/encoding.py
+++ src/xradio/vis/_vis_utils/_zarr/encoding.py
@@ -1,3 +1,5 @@
+import dask.array as da
+
 def add_encoding(xds, compressor, chunks=None):
     if chunks is None:
         chunks = xds.sizes
@@ -6,5 +8,20 @@ def add_encoding(xds, compressor, chunks=None):
 
     encoding = {}
     for da_name in list(xds.data_vars):
-        da_chunks = [chunks[dim_name] for dim_name in xds[da_name].sizes]
-        xds[da_name].encoding = {"compressor": compressor, "chunks": da_chunks}
+        if isinstance(xds[da_name].data, da.Array):
+            da_chunks = dict(xds[da_name].chunksizes)
+
+            # Loop over dimensions that can be chunked
+            for dim_name in chunks.keys():
+                # Only add user specified chunks to dimensions that aren't already chunked
+                # ie don't touch time on VISIBILITY when using `read_col_conversion_dask`
+                if len(da_chunks.get(dim_name, (1,))):
+                    da_chunks[dim_name] = chunks[dim_name]
+
+            xds[da_name].encoding = {"compressor": compressor, "chunks": da_chunks}
+
+        else:
+            for da_name in list(xds.data_vars):
+                da_chunks = [chunks[dim_name] for dim_name in xds[da_name].sizes]
+                xds[da_name].encoding = {"compressor": compressor, "chunks": da_chunks}
+

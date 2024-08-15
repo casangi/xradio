aa32d8c6c7e72db6c2b3664b259cb34777e00a36
Jan-Willem <jwsteeb@gmail.com>
2024-08-15 10:11:29 -0400

    Fix summary when ms_sel a single frequency .

--- src/xradio/vis/_processing_set.py
+++ src/xradio/vis/_processing_set.py
@@ -126,8 +126,8 @@ class processing_set(dict):
             summary_data["line_name"].append(value.attrs["partition_info"]["line_name"])
 
             # summary_data["num_lines"].append(value.attrs["partition_info"]["num_lines"])
-            summary_data["start_frequency"].append(value["frequency"].values[0])
-            summary_data["end_frequency"].append(value["frequency"].values[-1])
+            summary_data["start_frequency"].append(to_list(value["frequency"].values)[0])
+            summary_data["end_frequency"].append(to_list(value["frequency"].values)[-1])
 
             if value[data_name].attrs["field_and_source_xds"].is_ephemeris:
                 summary_data["field_coords"].append("Ephemeris")

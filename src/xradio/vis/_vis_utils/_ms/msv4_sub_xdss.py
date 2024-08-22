21ec96c2d170a66557085bdfd4bae0e0e5613f6e
Peter Wortmann <peter.wortmann@skao.int>
2024-08-22 15:51:28 +0100

    Use sky_dir_label instead of direction

--- src/xradio/vis/_vis_utils/_ms/msv4_sub_xdss.py
+++ src/xradio/vis/_vis_utils/_ms/msv4_sub_xdss.py
@@ -147,9 +147,9 @@ def create_ant_xds(
     ]
 
     # ['OFFSET', 'POSITION', 'DISH_DIAMETER', 'FLAG_ROW', 'MOUNT', 'NAME', 'STATION']
-    ant_xds = xr.Dataset(
-        coords={"antenna_id": antenna_id, "cartesian_pos_label": ["x", "y", "z"]},
-        attrs={'type':'antenna'}
+    ant_xds = xr.Dataset(attrs={'type': 'antenna'})
+    ant_xds = ant_xds.assign_coords(
+        {"antenna_id": antenna_id, "cartesian_pos_label": ["x", "y", "z"]}
     )
 
     coords = {}
@@ -168,8 +168,8 @@ def create_ant_xds(
                 da_attrs["type"] = "quantity"
 
             # Add data variable
-            data_vars[to_new_data_variable_names[key]] = xr.DataArray(
-                generic_ant_xds[key].data,
+            ant_xds[to_new_data_variable_names[key]] = xr.Variable(
+                data=generic_ant_xds[key].data,
                 dims=data_variable_dims[key],
                 attrs=da_attrs,
             )
@@ -441,7 +441,7 @@ def create_pointing_xds(
         "OVER_THE_TOP": "OVER_THE_TOP",
     }
     time_ant_dims = ["time", "antenna_id"]
-    time_ant_dir_dims = time_ant_dims + ["direction"]
+    time_ant_dir_dims = time_ant_dims + ["sky_dir_label"]
     data_variable_dims = {
         # "name": ["time", "antenna_id"],   # removed
         # "time_origin": ["time", "antenna_id"],   # removed?
@@ -533,7 +533,7 @@ def create_pointing_xds(
     coords = {
         "time": generic_pointing_xds["TIME"].values,
         "antenna_id": np.arange(generic_pointing_xds.sizes["ANTENNA_ID"]),
-        "direction": ["ra", "dec"],
+        "sky_dir_label": ["ra", "dec"],
     }
     pointing_xds = pointing_xds.assign_coords(coords)
 
@@ -550,7 +550,7 @@ def create_pointing_xds(
     if move_target_as_attr:
         target = generic_pointing_xds.data_vars["TARGET"]
         pointing_xds.attrs["target"] = {
-            "dims": ["direction"],
+            "dims": ["sky_dir_label"],
             "data": target.values[0, 0].tolist(),
             "attrs": column_description_casacore_to_msv4_measure(
                 pointing_column_descriptions["TARGET"]

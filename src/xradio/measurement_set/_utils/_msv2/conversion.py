72b50f6f4ed61af59401e0a78b011c23478bf0e3
Federico Montesino Pouzols <fpouzols@eso.org>
2024-11-04 13:41:16 +0100

    use timezone, datetime.UTC alias only in >=3.11, #263

--- src/xradio/measurement_set/_utils/_msv2/conversion.py
+++ src/xradio/measurement_set/_utils/_msv2/conversion.py
@@ -983,7 +983,9 @@ def convert_and_write_partition(
             start = time.time()
             xds = xr.Dataset(
                 attrs={
-                    "creation_date": datetime.datetime.now(datetime.UTC).isoformat(),
+                    "creation_date": datetime.datetime.now(
+                        datetime.timezone.utc
+                    ).isoformat(),
                     "xradio_version": importlib.metadata.version("xradio"),
                     "schema_version": "4.0.-9994",
                     "type": "visibility",

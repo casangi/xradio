f110cbab94004936ec1f1a1d40dbcc88381796fc
Federico Montesino Pouzols <fpouzols@eso.org>
2025-01-16 17:06:04 +0100

    clarify use of mixed_polarizations/polarization, #330

--- src/xradio/measurement_set/schema.py
+++ src/xradio/measurement_set/schema.py
@@ -1883,7 +1883,10 @@ class VisibilityXds:
     # --- Optional Coordinates ---
     polarization_mixed: Optional[Coord[tuple[BaselineId, Polarization], str]] = None
     """
-    If the polarizations are not constant over baseline
+    If the polarizations are not constant over baseline. For mixed polarizations one would
+    use ['PP', 'PQ', 'QP', 'QQ'] as the polarization labels and then specify here the
+    actual polarization basis for each baseline using labels from the set of all
+    combinations of 'X', 'Y', 'R' and 'L'.
     """
     uvw_label: Optional[Coordof[UvwLabelArray]] = None
     """ u,v,w """
@@ -1979,7 +1982,10 @@ class SpectrumXds:
     # --- Optional Coordinates ---
     polarization_mixed: Optional[Coord[tuple[AntennaName, Polarization], str]] = None
     """
-    If the polarizations are not constant over baseline
+    If the polarizations are not constant over antennas. For mixed polarizations one would
+    use ['PP', 'PQ', 'QP', 'QQ'] as the polarization labels and then specify here the
+    actual polarization basis for each antenna using labels from the set of
+    combinations of 'X', 'Y', 'R' and 'L'.
     """
     scan_number: Optional[Coord[Time, Union[numpy.int64, numpy.int32]]] = None
     """Arbitary scan number to identify data taken in the same logical scan."""

abd83abcb1a2b08698b19ba1d5a28e607cddda26
Federico Montesino Pouzols <fpouzols@eso.org>
2025-07-31 18:28:38 +0200

    try to load flags for multiple dims alternatives, #454

--- src/xradio/measurement_set/_utils/_asdm/_utils/bdf_load_data_flags.py
+++ src/xradio/measurement_set/_utils/_asdm/_utils/bdf_load_data_flags.py
@@ -60,7 +60,7 @@ def check_cross_and_auto_data_dims(bdf_header: pyasdm.bdf.BDFHeader) -> bool:
     return appears_single_dish
 
 
-def define_visibility_flag_shape(
+def define_visibility_shape(
     bdf_header: pyasdm.bdf.BDFHeader,
     baseband_description: dict,
     appears_single_dish: bool,
@@ -116,17 +116,16 @@ def load_visibilities(bdf_path: str, spw_id: int, array_slice: dict) -> np.ndarr
     ensure_presence_data_arrays(["crossData", "autoData"], bdf_header, bdf_path)
 
     scale_factor = baseband_description["scaleFactor"] or 1
-    shape = define_visibility_flag_shape(
+    shape = define_visibility_shape(
         bdf_header, baseband_description, appears_single_dish
     )
     cumulative_vis = []
-    count = 0
     while bdf_reader.hasSubset():
         try:
             subset = bdf_reader.getSubset()
         except ValueError as exc:
             logger.warning(f"Error in getSubset for {bdf_path=} {exc=}")
-            return np.zeros(1)
+            return np.zeros(shape)
 
         if "crossData" in subset and subset["crossData"]["present"]:
             # assuming dims ['BAL', 'BAB', 'SPP', 'POL']
@@ -143,10 +142,32 @@ def load_visibilities(bdf_path: str, spw_id: int, array_slice: dict) -> np.ndarr
             vis_subset = np.zeros(shape)
 
         cumulative_vis.append(vis_subset)
-        count += 1
 
     bdf_vis = np.concatenate(cumulative_vis)
     return bdf_vis
+
+
+def define_flag_shape(
+    bdf_header: pyasdm.bdf.BDFHeader,
+    baseband_description: dict,
+    appears_single_dish: bool,
+) -> tuple:
+
+    baseband_len = len(bdf_header.getBasebandsList())
+    antenna_len = bdf_header.getNumAntenna()
+    baseline_len = int(antenna_len * (antenna_len - 1) / 2)
+    frequency_len = baseband_description["numSpectralPoint"]
+    polarization_len = len(baseband_description["crossPolProducts"]) or len(
+        baseband_description["sdPolProducts"]
+    )
+    flags_dims = bdf_header.getAxesNames("flags")
+
+    if flags_dims in [["BAL", "BAB", "POL"], ["BAL", "ANT", "BAB", "POL"]]:
+        shape = (1, baseline_len, antenna_len, baseband_len, polarization_len)
+    elif flags_dims in [["ANT", "BAB", "BIN", "POL"]]:
+        shape = (1, antenna_len, baseband_len, frequency_len, polarization_len)
+
+    return shape
 
 
 def load_flags_from_bdfs(
@@ -158,7 +179,7 @@ def load_flags_from_bdfs(
 
     all_flag = []
     for bdf_path in bdf_paths:
-        all_flag.append(load_vis_flags_from_bdf(bdf_path, spw_id, array_slice))
+        all_flag.append(load_flags(bdf_path, spw_id, array_slice))
 
     return np.concatenate(all_flag)
 
@@ -168,10 +189,35 @@ def load_flags(bdf_path: list[str], spw_id: int, array_slice: dict) -> np.ndarra
     bdf_reader = pyasdm.bdf.BDFReader()
     bdf_reader.open(bdf_path)
     bdf_header = bdf_reader.getHeader()
+
+    appears_single_dish = check_cross_and_auto_data_dims(bdf_header)
+    basebands = bdf_header.getBasebandsList()
+    spw_baseband_num, baseband_description = find_spw_in_basebands_list(
+        bdf_path, spw_id, basebands
+    )
+
     ensure_presence_data_arrays(["flags"], bdf_header, bdf_path)
+
+    shape = define_flag_shape(bdf_header, baseband_description, appears_single_dish)
+    flag_dims = bdf_header.getAxesNames("flags")
+    cumulative_flag = []
     while bdf_reader.hasSubset():
         try:
             subset = bdf_reader.getSubset()
         except ValueError as exc:
             logger.warning(f"Error in getSubset for {bdf_path=} {exc=}")
-            return np.zeros(1)
+            return np.zeros(shape)
+
+        if "flags" in subset and subset["flags"]["present"]:
+            flag_array = subset["flags"]["arr"].reshape(shape)
+            if "BIN" in flag_dims:
+                flag_subset = flag_array[:, :, spw_baseband_num, :, :]
+            else:
+                flag_subset = flag_array[:, :, :, spw_baseband_num, :]
+        else:
+            flag_subset = np.zeros(shape, dtype="bool")[:, :, spw_baseband_num, :, :]
+
+        cumulative_flag.append(flag_subset)
+
+    bdf_flag = np.concatenate(cumulative_flag)
+    return bdf_flag

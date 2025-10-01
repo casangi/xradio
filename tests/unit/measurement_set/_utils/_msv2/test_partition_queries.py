9f30c97d2d0ce530d88f6d51b563f9e953429d70
Federico Montesino Pouzols <fpouzols@eso.org>
2025-10-01 09:53:32 +0200

    update unit tests for new create_partitions, #490

--- tests/unit/measurement_set/_utils/_msv2/test_partition_queries.py
+++ tests/unit/measurement_set/_utils/_msv2/test_partition_queries.py
@@ -12,7 +12,28 @@ expected_partition_axes = [
     "SOURCE_ID",
     "OBS_MODE",
     "SUB_SCAN_NUMBER",
+    "EPHEMERIS_ID",
 ]
+
+
+def check_expected_min_partitions(partitions: list[dict], expected_len: int = 4):
+    """Checks consistency of the basic partitions of the
+    ms_minimal_required test MS (1 per DDI)"""
+
+    assert isinstance(partitions, list)
+    assert len(partitions) == expected_len
+
+    assert all(
+        [axis in part for axis in expected_partition_axes for part in partitions]
+    )
+
+    assert all(
+        isinstance(part[axis], list) and len(part[axis]) == 1
+        for axis in expected_partition_axes
+        for part in partitions
+    )
+    ddis = {part["DATA_DESC_ID"][0] for part in partitions}
+    assert ddis == {0, 1, 2, 3}
 
 
 def test_create_partitions_ms_empty(ms_empty_required):
@@ -20,7 +41,7 @@ def test_create_partitions_ms_empty(ms_empty_required):
     assert parts == []
 
 
-def test_create_paritions_ms_min(ms_minimal_required):
+def test_create_partitions_ms_min(ms_minimal_required):
 
     parts = create_partitions(ms_minimal_required.fname, [])
     assert isinstance(parts, list)
@@ -31,25 +52,19 @@ def test_create_paritions_ms_min(ms_minimal_required):
 def test_create_partitions_ms_min_with_field(ms_minimal_required):
 
     parts = create_partitions(ms_minimal_required.fname, ["FIELD_ID"])
-    assert isinstance(parts, list)
-    assert len(parts) == 4
-    assert all([axis in part for axis in expected_partition_axes for part in parts])
+    check_expected_min_partitions(parts, 4)
 
 
 def test_create_partitions_ms_min_with_antenna1(ms_minimal_required):
 
     parts = create_partitions(ms_minimal_required.fname, ["ANTENNA1"])
-    assert isinstance(parts, list)
-    assert len(parts) == 16
-    assert all([axis in part for axis in expected_partition_axes for part in parts])
+    check_expected_min_partitions(parts, 16)
 
 
 def test_create_partitions_ms_min_with_state_id(ms_minimal_required):
 
     parts = create_partitions(ms_minimal_required.fname, ["STATE_ID"])
-    assert isinstance(parts, list)
-    assert len(parts) == 4
-    assert all([axis in part for axis in expected_partition_axes for part in parts])
+    check_expected_min_partitions(parts, 4)
 
 
 def test_create_partitions_ms_min_with_all(ms_minimal_required):
@@ -65,21 +80,14 @@ def test_create_partitions_ms_min_with_all(ms_minimal_required):
             "ANTENNA1",
         ],
     )
-    assert isinstance(parts, list)
-    assert len(parts) == 16
-    assert all([axis in part for axis in expected_partition_axes for part in parts])
+    check_expected_min_partitions(parts, 16)
 
 
 def test_create_partitions_ms_min_with_other(ms_minimal_required):
 
-    with pytest.raises(IndexError, match="out of range"):
-        parts = create_partitions(
-            ms_minimal_required.fname,
-            [
-                "FIELD_ID",
-                "SOURCE_ID",
-                "DATA_DESC_ID",
-            ],
-        )
+    parts = create_partitions(
+        ms_minimal_required.fname,
+        ["FIELD_ID", "SOURCE_ID", "DATA_DESC_ID"],
+    )
 
-        assert parts
+    check_expected_min_partitions(parts, 4)

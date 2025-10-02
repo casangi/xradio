c4636bfd4d5abb6f38987169a5577df3c574486a
Jan-Willem <jwsteeb@gmail.com>
2025-10-02 11:32:00 -0400

    Update docs tring from intents to scan_intents.

--- src/xradio/measurement_set/open_processing_set.py
+++ src/xradio/measurement_set/open_processing_set.py
@@ -14,8 +14,8 @@ def open_processing_set(
     ps_store : str
         String of the path and name of the processing set. For example '/users/user_1/uid___A002_Xf07bba_Xbe5c_target.lsrk.vis.zarr'.
     scan_intents : list, optional
-        A list of intents to be opened for example ['OBSERVE_TARGET#ON_SOURCE']. The intents in a processing_set_xdt can be seen by calling processing_set_xdt.ps.summary().
-        By default None, which will include all intents.
+        A list of scan_intents to be opened for example ['OBSERVE_TARGET#ON_SOURCE']. The scan_intents in a processing_set_xdt can be seen by calling processing_set_xdt.ps.summary().
+        By default None, which will include all scan_intents.
 
     Returns
     -------

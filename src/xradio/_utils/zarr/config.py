548acf144fbf09eb0e7fc44167b1e66e6a741405
Federico Montesino Pouzols <fpouzols@eso.org>
2026-01-22 15:58:01 +0100

    extend a bit doc of ZARR_FORMAT option, #355

--- src/xradio/_utils/zarr/config.py
+++ src/xradio/_utils/zarr/config.py
@@ -1,2 +1,4 @@
-# Added for the transition from zarr-python 2.x to 3.x
+# Added for the transition from zarr-python 2.x to 3.x. Zarr-Python v3 still supports
+# Zarr format v2 in addition to the new v3. This config option is set for the new/default
+# format v3 but can be changed to use format v2.
 ZARR_FORMAT = 3

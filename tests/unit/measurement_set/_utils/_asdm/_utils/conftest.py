4eb3851eaf06b9f295f7f148c0bd24a25817fbd4
Federico Montesino Pouzols <fpouzols@eso.org>
2026-01-29 18:28:06 +0100

    add more spw rows and test cases, #454

--- tests/unit/measurement_set/_utils/_asdm/_utils/conftest.py
+++ tests/unit/measurement_set/_utils/_asdm/_utils/conftest.py
@@ -27,7 +27,7 @@ def asdm_with_spw_default():
     return make_asdm_with_spw_default()
 
 def make_asdm_with_spw_simple():
-    spw_row_spec = """
+    spw_row_spec_0 = """
   <row>
     <spectralWindowId> SpectralWindow_0 </spectralWindowId>
     <basebandName>BB_1</basebandName>
@@ -47,12 +47,39 @@ def make_asdm_with_spw_simple():
     <assocSpectralWindowId> 1 1 SpectralWindow_0  </assocSpectralWindowId>
   </row>
     """
+    spw_row_spec_8 = """
+  <row>
+    <spectralWindowId> SpectralWindow_8 </spectralWindowId>
+    <basebandName>BB_1</basebandName>
+    <netSideband>LSB</netSideband>
+    <numChan> 128 </numChan>
+    <numBin> 1 </numBin>
+    <refFreq> 9.702100190734863E10 </refFreq>
+    <sidebandProcessingMode>NONE</sidebandProcessingMode>
+    <totBandwidth> 2.0E9 </totBandwidth>
+    <windowFunction>HANNING</windowFunction>
+    <chanFreqStart> 9.701318940734863E10 </chanFreqStart>
+    <chanFreqStep> -1.5625E7 </chanFreqStep>
+    <chanWidth> 1.5625E7 </chanWidth>
+    <effectiveBw> 4.1671875E7 </effectiveBw>
+    <name> X0000000000#ALMA_RB_03#BB_1#SW-01#FULL_RES </name>
+    <quantization> false </quantization>
+    <refChan> -0.5 </refChan>
+    <resolution> 3.125E7 </resolution>
+    <numAssocValues> 4 </numAssocValues>
+    <assocNature> 1 4 BASEBAND_WIDE BASEBAND_WIDE BASEBAND_WIDE CHANNEL_AVERAGE</assocNature>
+    <assocSpectralWindowId> 1 4 SpectralWindow_5 SpectralWindow_6 SpectralWindow_7 SpectralWindow_9  </assocSpectralWindowId>
+  </row>
+    """
     test_asdm = pyasdm.ASDM()
     test_spw_table = test_asdm.getSpectralWindow()
-    spw_row = pyasdm.SpectralWindowRow(test_spw_table)
-    spw_row.setFromXML(spw_row_spec)
-    test_spw_table.add(spw_row)
-    assert test_spw_table.size() == 1
+    spw_row_0 = pyasdm.SpectralWindowRow(test_spw_table)
+    spw_row_0.setFromXML(spw_row_spec_0)
+    test_spw_table.add(spw_row_0)
+    spw_row_8 = pyasdm.SpectralWindowRow(test_spw_table)
+    spw_row_8.setFromXML(spw_row_spec_8)
+    test_spw_table.add(spw_row_8)
+    assert test_spw_table.size() == 2
     return test_asdm
     
 @pytest.fixture(scope="session")
--- tests/unit/measurement_set/_utils/_asdm/_utils/test_spectral_window.py
+++ tests/unit/measurement_set/_utils/_asdm/_utils/test_spectral_window.py
@@ -32,14 +32,56 @@ def test_get_spw_frequency_centers_empty(asdm_empty):
     with pytest.raises(AttributeError, match="has no attribute"):
         centers = get_spw_frequency_centers(asdm_empty, 0, 64)
 
+def test_get_spw_frequency_centers_default(asdm_with_spw_default):
+    from xradio.measurement_set._utils._asdm._utils.spectral_window import get_spw_frequency_centers
+
+    with pytest.raises(NameError, match="chanFreqArray"):
+        centers = get_spw_frequency_centers(asdm_with_spw_default, 0, 1)
+
+def test_get_spw_frequency_centers_simple(asdm_with_spw_simple):
+    from xradio.measurement_set._utils._asdm._utils.spectral_window import get_spw_frequency_centers
+
+    centers_0 = get_spw_frequency_centers(asdm_with_spw_simple, 0, 1)
+    assert centers_0 == [85021000000.0]
+    centers_1 = get_spw_frequency_centers(asdm_with_spw_simple, 1, 128)
+    assert len(centers_1) == 128
+    assert centers_1.max() == 97013189407.34863
+    assert centers_1.min() == 95028814407.34863
+    with pytest.raises(AttributeError, match="has no attribute"):
+        get_spw_frequency_centers(asdm_with_spw_simple, 8, 128)
+
 def test_get_chan_width_empty(asdm_empty):
     from xradio.measurement_set._utils._asdm._utils.spectral_window import get_chan_width
 
     with pytest.raises(AttributeError, match="has no attribute"):
         chan_width = get_chan_width(asdm_empty, 0)
 
-def test_get_reference_frame(asdm_empty):
+def test_get_chan_width_default(asdm_with_spw_default):
+    from xradio.measurement_set._utils._asdm._utils.spectral_window import get_chan_width
+
+    with pytest.raises(NameError, match="chanWidthArray"):
+        chan_width = get_chan_width(asdm_with_spw_default, 0)
+
+def test_get_chan_width_simple(asdm_with_spw_simple):
+    from xradio.measurement_set._utils._asdm._utils.spectral_window import get_chan_width
+
+    chan_width = get_chan_width(asdm_with_spw_simple, 0)
+    assert chan_width == 2000000000.0
+
+def test_get_reference_frame_empty(asdm_empty):
     from xradio.measurement_set._utils._asdm._utils.spectral_window import get_reference_frame
 
     with pytest.raises(AttributeError, match="has no attribute"):
         ref_frame = get_reference_frame(asdm_empty, 0)
+
+def test_get_reference_frame_default(asdm_with_spw_default):
+    from xradio.measurement_set._utils._asdm._utils.spectral_window import get_reference_frame
+
+    ref_frame = get_reference_frame(asdm_with_spw_default, 0)
+    assert ref_frame == "TOPO"
+
+def test_get_reference_frame_simple(asdm_with_spw_simple):
+    from xradio.measurement_set._utils._asdm._utils.spectral_window import get_reference_frame
+
+    ref_frame = get_reference_frame(asdm_with_spw_simple, 0)
+    assert ref_frame == "TOPO"

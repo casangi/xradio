28a3726e4fe6436e679a1679cebe01f4824ac0eb
Federico Montesino Pouzols <fpouzols@eso.org>
2026-02-03 18:18:47 +0100

    some basic tests for _bdf/time #454

--- tests/unit/measurement_set/_utils/_asdm/_utils/_bdf/test_time.py
+++ tests/unit/measurement_set/_utils/_asdm/_utils/_bdf/test_time.py
@@ -24,3 +24,23 @@ def test_make_blob_info_empty():
     info = make_blob_info(BDFHeader())
     assert isinstance(info, pd.DataFrame)
     assert info.shape == (1, 22)
+
+
+def test_load_times_from_bdfs_empty():
+    from xradio.measurement_set._utils._asdm._utils._bdf.time import (
+        load_times_from_bdfs,
+    )
+
+    from pyasdm.exceptions import BDFReaderException
+
+    with pytest.raises(BDFReaderException, match="No such file or directory"):
+        load_times_from_bdfs(["/path/nonexistant/foo", "/path/nonexistant/bar"])
+
+
+def test_load_times_bdf_empty():
+    from xradio.measurement_set._utils._asdm._utils._bdf.time import load_times_bdf
+
+    from pyasdm.exceptions import BDFReaderException
+
+    with pytest.raises(BDFReaderException, match="No such file or directory"):
+        load_times_bdf("/path/nonexistant/foo")

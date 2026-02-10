7afbec8f0616ccc6bd6f684a797ce55f52e98e84
Federico Montesino Pouzols <fpouzols@eso.org>
2026-02-10 18:24:28 +0100

    monkeypatches and input tables (main/dd/pol) for full create coords tests, #454

--- tests/unit/measurement_set/_utils/_asdm/test_open_partition.py
+++ tests/unit/measurement_set/_utils/_asdm/test_open_partition.py
@@ -9,6 +9,61 @@ import pytest
 import pyasdm
 
 
+def add_main_table(asdm: pyasdm.ASDM):
+    main_row_0_xml = """
+  <row>
+    <time> 5230000651200000000 </time>
+    <numAntenna> 12 </numAntenna>
+    <timeSampling>INTEGRATION</timeSampling>
+    <interval> 24192000000 </interval>
+    <numIntegration> 1512 </numIntegration>
+    <scanNumber> 1 </scanNumber>
+    <subscanNumber> 1 </subscanNumber>
+    <dataSize> 1681962 </dataSize>
+    <dataUID>
+      <EntityRef entityId="uid://A002/X11b94a6/X119f" partId="X00000000" entityTypeName="Main" documentVersion="1"/>
+    </dataUID>
+    <configDescriptionId> ConfigDescription_0 </configDescriptionId>
+    <execBlockId> ExecBlock_0 </execBlockId>
+    <fieldId> Field_0 </fieldId>
+    <stateId> 1 12 State_0 State_0 State_0 State_0 State_0 State_0 State_0 State_0 State_0 State_0 State_0 State_0  </stateId>
+  </row>
+    """
+    main_table = asdm.getMain()
+    main_row_0 = pyasdm.MainRow(main_table)
+    main_row_0.setFromXML(main_row_0_xml)
+    main_table.add(main_row_0)
+
+
+def add_data_description_table(asdm: pyasdm.ASDM):
+    data_description_row_0_xml = """
+  <row>
+    <dataDescriptionId> DataDescription_0 </dataDescriptionId>
+    <polOrHoloId> Polarization_0 </polOrHoloId>
+    <spectralWindowId> SpectralWindow_0 </spectralWindowId>
+  </row>
+    """
+    data_description_table = asdm.getDataDescription()
+    data_description_row_0 = pyasdm.DataDescriptionRow(data_description_table)
+    data_description_row_0.setFromXML(data_description_row_0_xml)
+    data_description_table.add(data_description_row_0)
+
+
+def add_polarization_table(asdm: pyasdm.ASDM):
+    polarization_row_0_xml = """
+  <row>
+    <polarizationId> Polarization_0 </polarizationId>
+    <numCorr> 2 </numCorr>
+    <corrType> 1 2 XX YY</corrType>
+    <corrProduct> 2 2 2 X X Y Y</corrProduct>
+  </row>
+    """
+    polarization_table = asdm.getPolarization()
+    polarization_row_0 = pyasdm.PolarizationRow(polarization_table)
+    polarization_row_0.setFromXML(polarization_row_0_xml)
+    polarization_table.add(polarization_row_0)
+
+
 def test_open_partition_none():
     from xradio.measurement_set._utils._asdm.open_partition import open_partition
 
@@ -88,6 +143,36 @@ def test_create_coordinates_with_spw_simple(asdm_with_spw_simple):
         pyasdm.exceptions.BDFReaderException, match="No such file or directory"
     ):
         coords = create_coordinates(asdm_with_spw_simple, partition_descr, False)
+
+
+def test_create_coordinates_patched_bdf_with_spw_simple(
+    asdm_with_spw_simple, monkeypatch
+):
+    from xradio.measurement_set._utils._asdm.open_partition import create_coordinates
+
+    def mock_get_times_from_bdfs(
+        bdf_paths: list[str], scans_metadata: pd.DataFrame
+    ) -> tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
+        return np.array([0.1]), np.array([1.0]), np.array([0.101]), np.array([1.0])
+
+    partition_descr = {
+        "fieldId": [0],
+        "scanNumber": [0],
+        "scanIntent": 0,
+        "configDescriptionId": [0],
+        "dataDescriptionId": [0],
+        "BDFPath": ["/nonexistent/bar"],
+    }
+
+    monkeypatch.setattr(
+        "xradio.measurement_set._utils._asdm.open_partition.get_times_from_bdfs",
+        mock_get_times_from_bdfs,
+    )
+
+    add_main_table(asdm_with_spw_simple)
+    add_data_description_table(asdm_with_spw_simple)
+    add_polarization_table(asdm_with_spw_simple)
+    coords = create_coordinates(asdm_with_spw_simple, partition_descr, False)
 
 
 def test_produce_uvw_data_var():

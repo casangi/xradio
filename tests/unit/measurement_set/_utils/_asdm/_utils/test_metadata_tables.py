52ec56fd9ddc1f616b153ea49198bc7ff90fa992
Federico Montesino Pouzols <fpouzols@eso.org>
2026-03-17 18:51:43 +0100

    test case for 2dim polarizationtypes array loading, #454

--- tests/unit/measurement_set/_utils/_asdm/_utils/test_metadata_tables.py
+++ tests/unit/measurement_set/_utils/_asdm/_utils/test_metadata_tables.py
@@ -1,6 +1,10 @@
+import copy
+
 import pandas as pd
 
 import pytest
+
+import pyasdm
 
 from xradio.measurement_set._utils._asdm._utils.metadata_tables import (
     exp_asdm_table_to_df,
@@ -103,7 +107,6 @@ def test_exp_asdm_table_to_df_asdm_with_spw_simple(asdm_with_spw_simple):
     cols = ["fieldId"]
     main_df = exp_asdm_table_to_df(asdm_with_spw_simple, "Main", cols)
     assert main_df.empty
-    print(f"{main_df=}")
     pd.testing.assert_frame_equal(
         main_df, pd.DataFrame([], columns=cols, dtype="float64")
     )
@@ -114,3 +117,75 @@ def test_exp_asdm_table_to_df_asdm_with_spw_simple(asdm_with_spw_simple):
     pd.testing.assert_frame_equal(
         spw_df, pd.DataFrame([[0], [1]], columns=["spectralWindowId"])
     )
+
+
+def add_feed_table(asdm: pyasdm.ASDM):
+    feed_row_0_xml = """
+  <row>
+    <feedId> 0 </feedId>
+    <timeInterval> 7226686294548387903 3993371484612775807 </timeInterval>
+    <numReceptor> 2 </numReceptor>
+    <beamOffset> 2 2 2 0.0 0.0 0.0 0.0  </beamOffset>
+    <focusReference> 2 2 3 -99999.0 -99999.0 -99999.0 -99999.0 -99999.0 -99999.0  </focusReference>
+    <polarizationTypes> 1 2 X Y</polarizationTypes>
+    <polResponse> 2 2 2 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0  </polResponse>
+    <receptorAngle> 1 2 -0.9346238144 0.6361725124  </receptorAngle>
+    <antennaId> Antenna_0 </antennaId>
+    <receiverId> 1 2 0 0  </receiverId>
+    <spectralWindowId> SpectralWindow_0 </spectralWindowId>
+  </row>
+    """
+    feed_table = asdm.getFeed()
+    feed_row_0 = pyasdm.FeedRow(feed_table)
+    feed_row_0.setFromXML(feed_row_0_xml)
+    feed_table.add(feed_row_0)
+
+
+def add_polarization_table(asdm: pyasdm.ASDM):
+    polarization_row_0_xml = """
+  <row>
+    <polarizationId> Polarization_0 </polarizationId>
+    <numCorr> 2 </numCorr>
+    <corrType> 1 2 XX YY</corrType>
+    <corrProduct> 2 2 2 X X Y Y</corrProduct>
+  </row>
+    """
+    polarization_table = asdm.getPolarization()
+    polarization_row_0 = pyasdm.PolarizationRow(polarization_table)
+    polarization_row_0.setFromXML(polarization_row_0_xml)
+    polarization_table.add(polarization_row_0)
+
+
+def test_exp_asdm_table_to_df_asdm_with_spw_simple_plus_feed(asdm_with_spw_simple):
+    asdm_with_feed = copy.deepcopy(asdm_with_spw_simple)
+    add_feed_table(asdm_with_feed)
+
+    cols = ["polarizationTypes"]
+    feed_df = exp_asdm_table_to_df(asdm_with_feed, "Feed", cols)
+    pd.testing.assert_frame_equal(
+        feed_df,
+        pd.DataFrame(
+            [
+                [
+                    [
+                        pyasdm.enumerations.PolarizationType.X,
+                        pyasdm.enumerations.PolarizationType.Y,
+                    ]
+                ]
+            ],
+            columns=["polarizationTypes"],
+        ),
+    )
+
+
+def test_exp_asdm_table_to_df_asdm_with_spw_simple_plus_polarization(
+    asdm_with_spw_simple,
+):
+    asdm_with_polarization = copy.deepcopy(asdm_with_spw_simple)
+    add_polarization_table(asdm_with_polarization)
+
+    cols = ["corrProduct"]
+    polarization_df = exp_asdm_table_to_df(asdm_with_polarization, "Polarization", cols)
+    assert polarization_df.columns == cols
+    assert polarization_df.shape == (1, 1)
+    assert polarization_df["corrProduct"].values[0] == [["X", "X", "Y", "Y"]]

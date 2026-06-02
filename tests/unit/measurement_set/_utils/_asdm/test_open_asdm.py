7ac1ff20e52b60eda37cdca440195ce025d00643
Federico Montesino Pouzols <fpouzols@eso.org>
2026-06-02 11:57:18 +0200

    exercise failed partitions in open_asdm, #454

--- tests/unit/measurement_set/_utils/_asdm/test_open_asdm.py
+++ tests/unit/measurement_set/_utils/_asdm/test_open_asdm.py
@@ -67,3 +67,45 @@ def test_open_asdm_with_spw_simple(mock_asdm_set_from_file, monkeypatch):
     for _msv4_name, msv4_xdt in enumerate(ps_xdt):
         assert isinstance(msv4_xdt, str)
     check_datatree(ps_xdt)
+
+
+def test_open_asdm_with_spw_simple_fails_some_partitions(
+    mock_asdm_set_from_file, monkeypatch
+):
+    """
+    Forces a failure when loading at least one partitoin, exercising partition-level error handling code in
+    open_asdm
+    """
+
+    def mock_unreliable_load_times_from_partition_bdfs(
+        bdf_paths: list[str], scans_metadata: pd.DataFrame
+    ) -> tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
+        if mock_unreliable_load_times_from_partition_bdfs.partition_count == 0:
+            raise RuntimeError("Some error when loading times from BDF")
+        mock_unreliable_load_times_from_partition_bdfs.partition_count += 1
+        return np.array([0.1]), np.array([1.0]), np.array([0.101]), np.array([1.0]), {}
+
+    mock_unreliable_load_times_from_partition_bdfs.partition_count = 0
+
+    monkeypatch.setattr(
+        "xradio.measurement_set._utils._asdm.open_asdm.pyasdm.ASDM.setFromFile",
+        mock_asdm_set_from_file,
+    )
+    monkeypatch.setattr(
+        "pyasdm.MainRow.getBDFPath", lambda bdf_paths: "/monkypatched_path/foo"
+    )
+    monkeypatch.setattr(
+        "xradio.measurement_set._utils._asdm.open_partition.load_times_from_partition_bdfs",
+        mock_unreliable_load_times_from_partition_bdfs,
+    )
+
+    ps_xdt = open_asdm(
+        "/unused_path/foo",
+        ["dataDescriptionId", "execBlockId", "fieldId", "scanIntent"],
+        include_processor_types=["CORRELATOR", "SPECTROMETER", "RADIOMETER"],
+    )
+    assert isinstance(ps_xdt, xr.DataTree)
+    assert ps_xdt.type == "processing_set"
+    for _msv4_name, msv4_xdt in enumerate(ps_xdt):
+        assert isinstance(msv4_xdt, str)
+    check_datatree(ps_xdt)

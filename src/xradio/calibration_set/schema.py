f871ae30124c448f0185f0fdad76a4d07270ef62
Des Small <small@jive.eu>
2026-08-06 13:50:04 +0200

    Updated calibration_set/schema.py in line with current thinking

--- src/xradio/calibration_set/schema.py
+++ src/xradio/calibration_set/schema.py
@@ -35,8 +35,6 @@ import numpy
 Direction = Literal["direction"]
 
 
-
-
 # Note: this isn't in measurement_set.schema but I'm pretty sure that's where it belongs
 @xarray_dataarray_schema
 class ReceptorLabelArray:
@@ -47,18 +45,16 @@ class ReceptorLabelArray:
     data: Data[ReceptorLabel, str]
 
 
-
 @xarray_dataarray_schema
-class AntennaCalibrationParameterArray:
+class AntennaReceptorCalibrationParameterArray:
     """
-    A baseclass for AntennaCalibrationParameterArrays; each
+    A baseclass for AntennaReceptorCalibrationParameterArrays; each
     subclass is allowed to be more specific about the Data type and
     required to specify its own units.
     """
 
     data: Data[
-        tuple[#Direction,
-            Time, AntennaName, Frequency, ReceptorLabel],
+        tuple[Direction, Time, AntennaName, Frequency, ReceptorLabel],  # Direction,
         Union[numpy.float32, numpy.float64, numpy.complex64, numpy.complex128],
     ]
 
@@ -69,7 +65,20 @@ class AntennaCalibrationParameterArray:
     receptor_label: Coordof[ReceptorLabelArray]
 
 
-    
+@xarray_dataarray_schema
+class AntennaPolarizationCalibrationParameterArray:
+    data: Data[
+        tuple[Direction, Time, AntennaName, Frequency, Polarization],  # Direction,
+        Union[numpy.float32, numpy.float64, numpy.complex64, numpy.complex128],
+    ]
+
+    direction: Coord[Direction, str]
+    time: Coordof[TimeCoordArray]
+    antenna_name: Coordof[AntennaNameArray]
+    frequency: Coordof[FrequencyArray]
+    receptor_label: Coordof[ReceptorLabelArray]
+
+
 @xarray_dataarray_schema
 class BaselineCalibrationParameterArray:
     """
@@ -88,18 +97,30 @@ class BaselineCalibrationParameterArray:
 
     long_name: Optional[Attr[str]] = "Baseline Calibration Parameter"
 
-    
+
 ############################################################
 ## We flesh out how this works for fringefit parameters.
 ##
 ## We state that all the parameter values are real, and they all derive
 ## their axes by inheritance from AntennaCalibrationParameterArray but
-## each of them has a different unit
+## each of them has a different unit.
+##
+## I am not thrilled that we override the Data type being careful to
+## match the superclass dimensions and changing only the type, though.
 ##############################################################
+
+##
+## Note: We would much prefer to say:
+##
+## PhaseCal = AntennaCalibrationParameterArrayFactory(AntennaCalibrationParameterArray,
+##                                                    long_name="Phase offset",
+##                                                    unit="rad")
+##
+## And then actually inline that instead in the FringeJones class
 
 
 @xarray_dataarray_schema
-class PhaseCalibrationParameterArray(AntennaCalibrationParameterArray):
+class PhaseCalibrationParameterArray(AntennaReceptorCalibrationParameterArray):
     """A phase-offset calibration parameter for use in (e.g.) fringe fit tables"""
 
     data: Data[
@@ -110,9 +131,8 @@ class PhaseCalibrationParameterArray(AntennaCalibrationParameterArray):
     units: Attr[UnitsRadians] = "rad"
 
 
-
 @xarray_dataarray_schema
-class DelayCalibrationParameterArray(AntennaCalibrationParameterArray):
+class DelayCalibrationParameterArray(AntennaReceptorCalibrationParameterArray):
     """A delay calibration parameter for use in (e.g.) fringe fit tables"""
 
     data: Data[
@@ -123,7 +143,7 @@ class DelayCalibrationParameterArray(AntennaCalibrationParameterArray):
 
 
 @xarray_dataarray_schema
-class RateCalibrationParameterArray(AntennaCalibrationParameterArray):
+class RateCalibrationParameterArray(AntennaReceptorCalibrationParameterArray):
     """A dimensionless delay-rate calibration parameter for use in (e.g.) fringe fit tables"""
 
     data: Data[
@@ -144,30 +164,38 @@ class ParameterErrorArray:
     """
 
     data: Data[
-        #Union[
+        Union[
             tuple[
                 Direction,
                 Time,
                 AntennaName,
                 Frequency,
                 ReceptorLabel,
-         #   ],
-         #   tuple[Direction, Time, BaselineId, Frequency, Polarization],
+            ],
+            tuple[
+                Direction,
+                Time,
+                AntennaName,
+                Frequency,
+                Polarization,
+            ],
+            tuple[Direction, Time, BaselineId, Frequency, Polarization],
         ],
         Union[numpy.float32, numpy.float64],
     ]
     direction: Coord[Direction, str]
     time: Coordof[TimeCoordArray]
     antenna_name: Coordof[AntennaNameArray]
-#    baseline_id: Coordof[BaselineArray]
+    baseline_id: Coordof[BaselineArray]
     frequency: Coordof[FrequencyArray]
     receptor_label: Coordof[ReceptorLabelArray]
-#    polarization: Coordof[PolarizationArray]
+    polarization: Coordof[PolarizationArray]
 
 
 # We expect that a given CalibrationFlagArray will have either
 # (a) both a baseline and a polarization dimension; OR
 # (b) both an antenna_name and a receptor_label dimension
+
 
 @xarray_dataarray_schema
 class CalibrationFlagArray:
@@ -185,6 +213,13 @@ class CalibrationFlagArray:
                 AntennaName,
                 Frequency,
                 ReceptorLabel,
+            ],
+            tuple[
+                Direction,
+                Time,
+                AntennaName,
+                Frequency,
+                Polarization,
             ],
             tuple[
                 Direction,
@@ -211,10 +246,11 @@ class CalibrationFlagArray:
 
 @xarray_dataset_schema
 class FringeJonesXds:
-    """ A class for the FringeJonesXds, as used in the fringefit task.
+    """A class for the FringeJonesXds, as used in the fringefit task.
 
     Each calibration type will need to specify its own Xds in this way.
     """
+
     # --- Required data variables ---
     CALPARAM_PHASE: Dataof[PhaseCalibrationParameterArray]
     CALPARAM_DELAY: Dataof[DelayCalibrationParameterArray]
@@ -224,6 +260,7 @@ class FringeJonesXds:
     CALERROR_DELAY: Dataof[ParameterErrorArray]
     CALERROR_RATE: Dataof[ParameterErrorArray]
 
+    SNR: Dataof[ParameterErrorArray]
     # --- Required Coordinates ---
     direction: Coord[Direction, str]
     time: Coordof[TimeCoordArray]
@@ -289,10 +326,11 @@ class FringeJonesXds:
     reference_antenna: Optional[Attr[str]] = None
     """The reference antenna (if any) used for this calibration."""
 
+
 ## The BaselineCalibrationXds is now obsolete, but I am not ready to
 ## delete it yet; I need a concrete BaslineCalibration example fleshed
 ## out before I let it go.
-    
+
 # @xarray_dataset_schema
 # class BaselineCalibrationXds:
 #     """Calibration dataset for baseline effects"""
@@ -340,7 +378,7 @@ class FringeJonesXds:
 #     creation_date: Attr[str]
 #     """Date calibration dataset was created. Format: YYYY-MM-DDTHH:mm:ss.SSS (ISO 8601)"""
 
-#     type: Attr[Literal["antenna_calibration", "baseline_calibration"]] = (
+# type: Attr[Literal["antenna_calibration", "baseline_calibration"]] = (
 #         "baseline_calibration"
 #     )
 #     """
@@ -361,5 +399,3 @@ class FringeJonesXds:
 #     """Scan name to identify data taken in the same logical scan"""
 
 #     # --- Optional Attributes ---
-
-    
